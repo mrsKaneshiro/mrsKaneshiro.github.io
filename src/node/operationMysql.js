5c3b63fbb2bf530110ecef34a9cff17da7e2e33b
gaoxin <360551737@qq.com>
2020-04-25 12:56:27 +0800

    update

--- src/node/operationMysql.js
+++ src/node/operationMysql.js
@@ -1,43 +1,59 @@
 //引入数据库
-console.log("start")
 const mysql = require("mysql")
 
 //实现本地链接
-var connection = createConnection({
-    host: 'default',
+var connection = mysql.createConnection({
+    host: 'localhost',
     user: 'root',
     password: 'cc931819',
     database: 'test'
 })
 
-connection.connect();
-
-//添加
-function add() {
-    let post = {
-        id: 2,
-        label: '妇产科',
-        text: '我这几次月经量比较少，而且痛经肚子痛，来例假的时候还会呕吐'
-    };
-    let query = connection.query("INSERT INTO demo SET ?", post, function (error, results, fields) {
-        if (error) throw error;
-    })
-    console.log(query.sql); //INSERT INTO posts 'id'=1, 'title'='Hello MySQL'
+function findData(){
+    var  sql = 'SELECT * FROM list';
+    connection.query(sql,function (err, result) {
+            if(err){
+            console.log('[SELECT ERROR] - ',err.message);
+            return;
+            }
+    
+        console.log('--------------------------SELECT----------------------------');
+        console.log(result);
+        console.log('------------------------------------------------------------\n\n');  
+    });
 }
 
+function add(id,label,text){
+    var  addSql = 'INSERT INTO list(id,label,text) VALUES(?,?,?)';
+    //var  addSqlParams = ['2','妇产科', '我这几次月经量比较少，而且痛经肚子痛，来例假的时候还会呕吐'];
+    var  addSqlParams = [id,label,text];
+    connection.query(addSql,addSqlParams,function (err, result) {
+            if(err){
+             console.log('[INSERT ERROR] - ',err.message);
+             return;
+            }        
+           console.log('--------------------------INSERT----------------------------');
+           console.log('INSERT ID:',result);        
+           console.log('-----------------------------------------------------------------\n\n');  
+    });
+}
 
-var  addSql = 'INSERT INTO list(id,label,text) VALUES(2,?,?)';
-var  addSqlParams = ['妇产科', '我这几次月经量比较少，而且痛经肚子痛，来例假的时候还会呕吐'];
-//增
-connection.query(addSql,addSqlParams,function (err, result) {
-        if(err){
-         console.log('[INSERT ERROR] - ',err.message);
-         return;
-        }        
- 
-       console.log('--------------------------INSERT----------------------------');
-       console.log('INSERT ID:',result);        
-       console.log('-----------------------------------------------------------------\n\n');  
-});
- 
+function getTotalNumber(){
+    var  sql = 'select * from list where id=(select MAX(id) from list )';
+    connection.query(sql,function(err, result){
+            if(err){
+            console.log('[SELECT ERROR] - ',err.message);
+            return;
+            }
+            var aaa = result[0].id
+    }.bind(this));
+}
+
+//数据库操作开始
+
+getTotalNumber()
+// var label = '儿科';
+// var text = '我家宝宝最近屁屁上张了很多斑点，有点像癣一样，还有点痒痒';
+// add(id,label,text)
 connection.end();
+
