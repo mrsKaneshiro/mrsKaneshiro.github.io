99a77dca3fd219d56caa8141795ce43113349984
gaoxin <360551737@qq.com>
2020-05-08 19:33:12 +0800

    update

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -22,10 +22,8 @@ class Sidebar extends React.Component {
         };
     }
 
-    toggleCollapsed(){
-        this.setState({
-            collapsed: !this.state.collapsed,
-        });
+    clickMenu(item, key, keyPath, domEvent){
+        console.log("click",item, key, keyPath, domEvent)
     }
   
     render() {
@@ -36,31 +34,17 @@ class Sidebar extends React.Component {
                     defaultOpenKeys={['sub1']}
                     mode="inline"
                     theme="dark"
+                    onClick={this.clickMenu}
                     inlineCollapsed={this.state.collapsed}
                 >
-                        <Menu.Item key="1">
-                            Option 1
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            Option 2
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            Option 3
-                        </Menu.Item>
-                        <SubMenu key="sub1" title="Navigation One">
-                            <Menu.Item key="5">Option 5</Menu.Item>
-                            <Menu.Item key="6">Option 6</Menu.Item>
-                            <Menu.Item key="7">Option 7</Menu.Item>
-                            <Menu.Item key="8">Option 8</Menu.Item>
+                        <SubMenu key="sub1" title="数据管理平台">
+                            <Menu.Item key="1">数据校验</Menu.Item>
+                            <Menu.Item key="2">Option 5</Menu.Item>
+                            <Menu.Item key="3">Option 6</Menu.Item>
+                            <Menu.Item key="4">Option 7</Menu.Item>
+                            <Menu.Item key="5">Option 8</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub2" title="Navigation Two">
-                            <Menu.Item key="9">Option 9</Menu.Item>
-                            <Menu.Item key="10">Option 10</Menu.Item>
-                            <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
-                            </SubMenu>
-                        </SubMenu>
+
                 </Menu>
             </div>
         )
